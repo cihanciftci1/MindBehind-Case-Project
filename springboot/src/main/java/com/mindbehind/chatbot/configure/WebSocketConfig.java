80e9ed3789b2f4852f2580c9359f19fcee398961
Cihan Ciftci <cihanciftci40@gmail.com>
2022-04-05 12:05:55 +0300

    Create PredefinedSentences and WebSocketConfig

---
+++ springboot/src/main/java/com/mindbehind/chatbot/configure/WebSocketConfig.java
@@ -0,0 +1,22 @@
+package com.mindbehind.chatbot.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/chatbot").setAllowedOrigins("*").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+    }
+
+}
---
+++ springboot/src/main/java/com/mindbehind/chatbot/utils/PredefinedSentences.java
@@ -0,0 +1,11 @@
+package com.mindbehind.chatbot.utils;
+
+public class PredefinedSentences {
+    public static String[] SENTENCES = {"Hi, Welcome to our company, we are here to assist you. Please contact us on: test@test.com",
+            "Hi, we are here to assist you Please contact us on: test@test.com"};
+
+
+
+
+
+}
